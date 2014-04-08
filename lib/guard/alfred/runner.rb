7800d7cc6f23f579c024289198d8885b220a39d3
Johan van Zonneveld <johan@vzonneveld.nl>
2014-04-08 20:54:46 +0200

    Remove messages

--- lib/guard/alfred/runner.rb
+++ lib/guard/alfred/runner.rb
@@ -6,8 +6,6 @@ module Guard
       # Runs all the scenario's.
       #
       def run_all
-        ::Guard::UI.info("Running all Alfred scenario's\n", :reset => true)
-
         Bundler.with_clean_env { Kernel.system('bundle exec alfred') }
       end
 
@@ -16,8 +14,6 @@ module Guard
       #
       def run(paths)
         return true if paths.empty?
-        ::Guard::UI.info("Alfred is looking for: #{paths.join(' ')}\n", :reset => true)
-
         Bundler.with_clean_env { Kernel.system("bundle exec alfred #{paths.join(' ')}") }
       end
 
